eefa7099115b82fb20773e2b3e801808e2dd681d
Tanjila Fariha <101992056+Tanjila-Fariha@users.noreply.github.com>
2024-04-07 21:34:05 +0600

    Add files via upload

---
+++ Resident_login.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+require_once("dbconnect.php");
+$Resident_ID = $_POST['Resident_ID'];
+$password = $_POST['password'];
+$type= $_POST['type'];
+if (empty($Resident_ID) or empty($password)) {
+	echo "required field is empty.";
+}
+else {
+	$query="SELECT * FROM resident where Resident_ID='$Resident_ID' AND password='$password'";
+	$result = $conn->query($query);
+	$row = $result->fetch_assoc();
+	if ($row) {
+		$_SESSION['Resident_ID'] = $row['Resident_ID'];
+		if ($type=='Flat Owner') {
+			header('Location: http://localhost/CSE370_project/Flat_Owner.html');
+		} elseif ($type=='Tenant') {
+			header('Location: http://localhost/CSE370_project/tenant.html');
+		}
+	}
+	else {
+		echo "Invalid Login Credentials";
+	}
+}
+?>
