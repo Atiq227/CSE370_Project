9fb815dd109772d4d1ea990aac31955be7c53ce4
Tanjila Fariha <101992056+Tanjila-Fariha@users.noreply.github.com>
2024-04-24 22:03:27 +0600

    Add files via upload

--- Remove_Flat.php
+++ Remove_Flat.php
@@ -3,8 +3,9 @@ session_start();
 require_once("dbconnect.php");
 $Resident_ID=$_SESSION['Resident_ID'];
 $Flat_ID = $_POST['Flat_ID'];
+$otp= $_POST['otp'];
 $flag=0;
-if (empty($Flat_ID)) {
+if (empty($Flat_ID) or empty($otp)) {
 	echo "required field is empty.";
 	$flag=1;
 	}
@@ -20,27 +21,20 @@ else {
 					while ($row1 = $result1->fetch_assoc()) {
 						if ($row1['O_Flat_ID']=== $Flat_ID) {
 							$flag=1;
-							$sql = "DELETE FROM owns WHERE O_Flat_ID='$Flat_ID'";
-							if ($conn->query($sql) == TRUE) {
+							$sql = "DELETE FROM owns WHERE O_Flat_ID='$Flat_ID';";
+							$sql .= "UPDATE apartment_a1 SET otp='$otp' WHERE Flat_ID='$Flat_ID'";
+
+							if ($conn->multi_query($sql) === TRUE) {
+                                do {
+                                    if ($result = $conn->store_result()) {
+                                        $result->free();
+                                    }
+                                } while ($conn->next_result());
 
 							  	echo "Flat_ID removed successfully";
-								$query2="SELECT O_Flat_ID FROM owns WHERE O_Resident_ID='$Resident_ID'";
-								$result2 = $conn->query($query2);
-								if ($result2->num_rows == 0) {
-									$sql1= "DELETE FROM resident where Resident_ID='$Resident_ID'";
-									if ($conn->query($sql1) == TRUE) {
-										header('Location: http://localhost/CSE370_project/Home_page.html');
-									} else {
-									  echo "Error: " . $sql . "<br>" . $conn->error;
-									}
-									$conn->close();
-								}
-
 							} else {
 							  echo "Error: " . $sql . "<br>" . $conn->error;
 							}
-							$conn->close();
-
 							break;
 						}
 					}
@@ -48,6 +42,18 @@ else {
 				break;
 			}
 		}
+	}
+	if ($flag==1) {
+		$query2="SELECT O_Flat_ID FROM owns WHERE O_Resident_ID='$Resident_ID'";
+		$result2 = $conn->query($query2);
+		if ($result2->num_rows == 0) {
+			$sql1= "DELETE FROM resident where Resident_ID='$Resident_ID'";
+			if ($conn->query($sql1) === TRUE) {
+				header('Location: http://localhost/CSE370_project/Home_page.html');
+			} else {
+			  echo "Error: " . $sql1 . "<br>" . $conn->error;
+			}
+		}		
 	}
 	if ($flag==0) {
 		echo "wrong Flat_ID";
