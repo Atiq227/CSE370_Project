3e681f4fe0ed3d30b18e2e4a7309eb897be75d9c
KhelaShikhi <166162881+KhelaShikhi@users.noreply.github.com>
2024-04-28 21:31:55 +0600

    Update complete_request.php

--- complete_request.php
+++ complete_request.php
@@ -10,7 +10,7 @@ $request_id = $_POST['request_id'];
 if(isset($_POST['complete_request'])) {
     $query = "DELETE FROM maintainence_request WHERE `Request_ID` = '$request_id'";
     if ($conn->query($query) === TRUE) {
-        echo "Complete ";
+        echo "Completed";
     }  
 }
 
